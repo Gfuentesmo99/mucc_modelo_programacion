7e9aad4a445fa5cfb69cd1a352693dde6ce30d2a
Gfuentesmo99 <gfuentesmo99@gmail.com>
2021-12-20 22:14:34 +0000

    Grafo 9 verices-2

--- grafos/test02.py
+++ grafos/test02.py
@@ -5,7 +5,7 @@ from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import EmbeddingComposite
 
 G = nx.Graph()
-G.add_nodes_from(["1","2","3","4","5","6"])
+G.add_nodes_from(["1","2","3","4","5","6","7","8","9"])
 G.add_edge("1","2")
 G.add_edge("1","3")
 G.add_edge("2","3")
@@ -15,9 +15,12 @@ G.add_edge("3","5")
 G.add_edge("4","5")
 G.add_edge("2","6")
 G.add_edge("4","6")
+G.add_edge("4","7")
+G.add_edge("2","8")
+G.add_edge("4","9")
 
 nx.draw(G, with_labels= True)
-plt.savefig("network6.png")
+plt.savefig("network8.png")
 
 sampler = EmbeddingComposite(DWaveSampler())
 print(dnx.min_vertex_cover(G,sampler))
