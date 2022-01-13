cf107e39c63a2d1a0989f7580df311459270e6e5
Gfuentesmo99 <gfuentesmo99@gmail.com>
2022-01-13 18:51:28 +0000

    Menu y pegasus

--- restricciones/restrictions_ej_21_12.py
+++ restricciones/restrictions_ej_21_12.py
@@ -4,7 +4,8 @@ from dwave.system.samplers import DWaveSampler
 from dwave.system.samplers import DWaveCliqueSampler
 from dwave.system.composites import EmbeddingComposite
 from dimod import BinaryQuadraticModel
-from dwave.system.samplers import LeapHybridSampler
+from dwave.system.samplers import LeapHybridBQMSampler
+import dwave.inspector
 
 ##############-- CONSTANTES --##############
 SEPARATOR = "------------------ "
@@ -49,9 +50,35 @@ def print_results(response, show_all):
 def execute_quantum(sampler, reads):
     response = sampler.sample(bqm, num_reads = reads)
     print_results(response, SHOW_ALL)
+    dwave.inspector.show(response)
+
+#Funcion que muestra el menu
+def menu():
+	print ("Selecciona una opción")
+	print ("\t1 - Sampler clasico\n\t2 - Dwave sampler\n\t3 -  Qimera sampler\n\t0 - Salir")
+
+#Solución con ordenador clásico
+def classic_sampler():
+    print_separator("Solucion con ordenador clasico")
+    sampler = ExactSolver()
+    sampleset = sampler.sample(bqm)
+    print(sampleset.lowest(atol=.5))
+
+#Solucion con ordenador cuantico - DWave Pegasus Advantage
+def dwave_sampler():
+    print_separator("Solucion con ordenador cuantico DWave Sampler")
+    sampler = EmbeddingComposite(DWaveSampler(solver={'topology__type': 'pegasus'}))
+    execute_quantum(sampler,1000)
+
+##Solucion con ordenador cuantico - DW 2000Q Quimera
+def qimera_sampler():
+    print_separator("Solucion con ordenador cuantico DW 2000Q")
+    sampler = EmbeddingComposite(DWaveSampler(solver='DW_2000Q_6'))
+    execute_quantum(sampler,1000)
 
 ##############-- MAIN --##############
-#Obtenemos los constraints
+####-- OBTENCION BQM --####
+#Obtenemos los constraints solucionando el problema manualmente
 bqm = BinaryQuadraticModel({'h':1,'u':1,'d':-1},{'hu':-2,'ha':-1,'hd':1},1,'BINARY')
 ## Otra forma de obtener el bqm
 #csp = dwavebinarycsp.ConstraintSatisfactionProblem(dwavebinarycsp.BINARY)
@@ -59,37 +86,23 @@ bqm = BinaryQuadraticModel({'h':1,'u':1,'d':-1},{'hu':-2,'ha':-1,'hd':1},1,'BINA
 #bqm = dwavebinarycsp.stitch(csp)
 #
 
-#Solución con ordenador clásico
-print_separator("Solucion con ordenador clasico")
-sampler = ExactSolver()
-sampleset = sampler.sample(bqm)
-print(sampleset.lowest(atol=.5))
+####-- EJECUCION --####
 
-#Solucion con ordenador cuantico - DWave Sampler
-print_separator("Solucion con ordenador cuantico DWave Sampler")
-sampler = EmbeddingComposite(DWaveSampler())
-execute_quantum(sampler,5000)
-
-#Solucion con ordenador cuantico - DwaveCliqueSampler -NO FUNCIONA
-#print_separator("Solucion con ordenador cuantico DWave Clique Sampler")
-#sampler = DWaveCliqueSampler() 
-#execute_quantum(sampler,100)
-
-##Solucion con ordenador cuantico - DW 2000Q
-print_separator("Solucion con ordenador cuantico DW 2000Q")
-sampler = EmbeddingComposite(DWaveSampler(solver='DW_2000Q_6'))
-execute_quantum(sampler,5000)
-
-##Solucion con ordenador cuantico - Dw Pegasus
-print_separator("Solucion con ordenador cuantico Dwave Pegasus")
-sampler = EmbeddingComposite(DWaveSampler(solver=dict(topology__type='pegasus')))
-execute_quantum(sampler,5000)
-
-##Solucion con ordenador cuantico - Dw Hybrid
-print_separator("Solucion con ordenador cuantico Dwave Hybrid")
-sampler = LeapHybridSampler()
-#execute_quantum(sampler,5000)
-response = sampler.sample(bqm)
-print(response)
-
+ 
+while True:
+	#Mostramos el menu
+	menu()
+	#Seleccionamos solver
+	opc=input("Selecciona un solver: ")
+	if opc=="1":
+		classic_sampler()
+	elif opc=="2":
+		dwave_sampler()
+	elif opc=="3":
+		qimera_sampler()
+	elif opc=="0":
+		break
+	else:
+		print ("")
+		input("No has pulsado ninguna opción correcta...\npulsa una tecla para continuar")
 
